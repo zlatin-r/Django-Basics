607ad667b593f1499f087b193323c0827bd6827d
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-15 13:30:42 +0200

    Changes Commited

--- Django101/Django101/asgi.py
+++ Django101/Django101/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Tasks project.
+ASGI config for Django101 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tasks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django101.settings')
 
 application = get_asgi_application()
