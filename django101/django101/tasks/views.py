f3ddbb0d0368b952ba9cdacd075eb92a609fae0b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-15 15:36:27 +0200

    Changes Commited

--- django101/django101/tasks/views.py
+++ django101/django101/tasks/views.py
@@ -9,31 +9,46 @@ from django101.tasks.models import Task
 
 # Create your views here.
 
+# def index(request):
+#     title_filter = request.GET.get('filter', '')
+#
+#     tasks = Task.objects.all()
+#
+#     if title_filter:
+#         tasks = tasks.filter(title__icontains=title_filter.lower())
+#
+#     result = []
+#
+#     if not tasks:
+#         return HttpResponse("<h1>No tasks !!!</h1>")
+#     else:
+#         for task in tasks:
+#             result.append(f"""
+#             <li>
+#                 <h2>{task.title}</h2>
+#                 <p>{task.description}</p>
+#             </li>""")
+#
+#     ul = f"<ul>{''.join(result)}</ul>"
+#
+#     content = f"""
+#         <h1>{len(tasks)} Tasks</h1>
+#         {ul}"""
+#
+#     return HttpResponse(content)
+
 def index(request):
-    title_filter = request.GET.get('filter', '')
+    title_filter = request.GET.get('filter', None)
 
     tasks = Task.objects.all()
 
     if title_filter:
         tasks = tasks.filter(title__icontains=title_filter.lower())
 
-    result = []
+    context = {
+        "title": "The tasks app!!!",
+        "task_list": tasks,
+        "tasks_count": tasks.count(),
+    }
 
-    if not tasks:
-        return HttpResponse("<h1>No tasks !!!</h1>")
-    else:
-        for task in tasks:
-            result.append(f"""
-            <li> 
-                <h2>{task.title}</h2>
-                <p>{task.description}</p>
-            </li>""")
-
-    ul = f"<ul>{''.join(result)}</ul>"
-
-    content = f"""
-        <h1>{len(tasks)} Tasks</h1>
-        {ul}"""
-
-    # return HttpResponse(content)
-    return render(request, 'tasks/index.html')
+    return render(request, 'tasks/index.html', context)
