fb18546e551832255f17d1a4ad28637d1d042517
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-15 13:34:32 +0200

    Changes Commited

--- django101/django101/asgi.py
+++ django101/django101/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Django101 project.
+ASGI config for django101 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django101.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django101.settings')
 
 application = get_asgi_application()
--- django101/django101/urls.py
+++ django101/django101/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Django101 project.
+URL configuration for django101 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
