334a4abb47a97bc5c3a37acf3e47f9aa34d73cf0
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-15 14:31:02 +0200

    Changes Commited

--- django101/django101/tasks/views.py
+++ django101/django101/tasks/views.py
@@ -1,3 +1,5 @@
+import http
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -9,9 +11,22 @@ from django101.tasks.models import Task
 def index(request):
     tasks = Task.objects.all()
 
-    output = "; ".join(f"{t.title}: {t.description}" for t in tasks)
+    result = []
 
-    if not output:
-        output = "No tasks"
+    if not tasks:
+        return HttpResponse("<h1>No tasks !!!</h1>")
+    else:
+        for task in tasks:
+            result.append(f"""
+            <li> 
+                <h2>{task.title}</h2>
+                <p>{task.description}</p>
+            </li>""")
 
-    return HttpResponse(output)
+    ul = f"<ul>{''.join(result)}</ul>"
+
+    content = f"""
+        <h1>{len(tasks)} Tasks</h1>
+        {ul}"""
+
+    return HttpResponse(content)
