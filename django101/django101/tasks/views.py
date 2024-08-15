853cfffb8efb3af8349a52bb9587e4516b8f5dac
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-15 13:56:55 +0200

    Changes Commited

--- django101/django101/tasks/views.py
+++ django101/django101/tasks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django101.tasks.models import Task
+
 
 # Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+
+    output = "; ".join(f"{t.title}: {t.description}" for t in tasks)
+
+    if not output:
+        output = "No tasks"
+
+    return HttpResponse(output)
