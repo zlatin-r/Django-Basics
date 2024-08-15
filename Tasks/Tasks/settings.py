3e350c3bca6163c3f41b9cedb73b5a20ef0c0a72
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-15 13:12:02 +0200

    Changes Commited

--- Tasks/Tasks/settings.py
+++ Tasks/Tasks/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'Tasks.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tasks_db",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
