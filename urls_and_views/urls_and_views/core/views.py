f2d4b334b3352e1a9d7fc88d82018f47c69eca98
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-18 13:31:58 +0200

    Changes Commited

--- urls_and_views/urls_and_views/core/views.py
+++ urls_and_views/urls_and_views/core/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
 def index(request, *args, **kwargs):
     return HttpResponse(f"It works with args={args} and kwargs={kwargs}")
--- urls_and_views/urls_and_views/departments/urls.py
+++ urls_and_views/urls_and_views/departments/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from urls_and_views.departments.views import department_details, department_details_by_name
 
 urlpatterns = (
     # path("departments/1/", department_1_details),
     # path("departments/2/", department_2_details),
+    path("", include("urls_and_views.core.urls")),
     path("<int:pk>/", department_details),
     path("<str:name>/", department_details_by_name),
 )
