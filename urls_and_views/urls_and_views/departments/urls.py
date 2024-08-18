7deacd2f716dd1b467d95771ded823d27e2c6337
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-18 09:59:37 +0200

    Changes Commited

---
+++ urls_and_views/urls_and_views/departments/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from urls_and_views.departments.views import department_details, department_details_by_name
+
+urlpatterns = (
+    # path("departments/1/", department_1_details),
+    # path("departments/2/", department_2_details),
+    path("<int:pk>/", department_details),
+    path("<str:name>/", department_details_by_name),
+)
--- urls_and_views/urls_and_views/urls.py
+++ urls_and_views/urls_and_views/urls.py
@@ -15,17 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from urls_and_views.departments.views import index, department_1_details, department_2_details, department_details, \
-    department_details_by_name
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index),
-    path("index/", index),
-    # path("departments/1/", department_1_details),
-    # path("departments/2/", department_2_details),
-    path("departments/<int:pk>/", department_details),
-    path("departments/<str:name>/", department_details_by_name),
+    path("departments/" , include("urls_and_views.urls")),
 ]
