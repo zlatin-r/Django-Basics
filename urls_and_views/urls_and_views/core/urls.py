c5665b7c866c06d96d33ccfbd80691eb8cec62af
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-18 14:37:13 +0200

    Changes Commited

--- urls_and_views/urls_and_views/core/urls.py
+++ urls_and_views/urls_and_views/core/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 
-from urls_and_views.core.views import index
+from urls_and_views.core.views import index, index_json
 
-urlpatterns = [
+urlpatterns = (
     path("<int:pk>/", index),
     path("<slug:slug>/", index),
     path("<int:pk>/<slug:slug>/", index),
-]
+
+    path('json/', include(
+    [
+        path('', index_json),
+        path('<int:pk>/', index_json),
+        path('<slug:slug>/', index_json),
+        path('<int:pk>/<slug:slug>/', index_json)
+    ])),
+)
+
--- urls_and_views/urls_and_views/core/views.py
+++ urls_and_views/urls_and_views/core/views.py
@@ -1,10 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request, *args, **kwargs):
-    return HttpResponse(f"It works with:<br/>"
-               f"args={args} and kwargs={kwargs}<br/>")
-               # f"method={request.method}<br/>"
-               # f"user={request.user}<br/>"
-               # f"path={request.path}<br/>")
+    content = (f"It works with:<br/>"
+               f"args={args} and kwargs={kwargs}<br/>"
+               f"method={request.method}<br/>"
+               f"user={request.user}<br/>"
+               f"path={request.path}<br/>")
 
+    return HttpResponse(
+        content,
+        content_type="application/json"
+    )
+
+def index_json(request, *args, **kwargs):
+    content = {
+        "args": args,
+        "kwargs": kwargs,
+        "path": request.path,
+        "method": request.method,
+        "user": str(request.user),
+    }
+
+    return JsonResponse(
+        content,
+        content_type="application/json",
+        # status=201,
+    )
