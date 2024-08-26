283dd4df2e22a665773e81adf8149f5c991ad2ce
zlatin-r <zlatinrusev@gmail.com>
2024-08-26 10:05:21 +0200

    Initial Commit

--- forms_advanced/forms_advanced/settings.py
+++ forms_advanced/forms_advanced/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "forms_advanced.web.apps.WebConfig"
+    "forms_advanced.web"
 ]
 
 MIDDLEWARE = [
