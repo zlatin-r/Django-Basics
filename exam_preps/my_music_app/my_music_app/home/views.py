63904aa766e11f07c03214c7d51f36c27c7c3163
Zlatin Rusev <zlatinrusev@gmail.com>
2024-09-17 15:24:27 +0200

    Changes Commited

--- exam_preps/my_music_app/my_music_app/home/views.py
+++ exam_preps/my_music_app/my_music_app/home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from my_music_app.common.profile_helpers import get_profile
+
+def create_profile(request):
+    return render(request, "home/home-no-profile.html")
 
 def index(request):
-    return render(request, "home/home-no-profile.html")
+    profile = get_profile()
+
+    if profile is not None:
+        return create_profile(request)
+
+    return render(request, "home/home-with-profile.html")
