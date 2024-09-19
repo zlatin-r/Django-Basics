cbcbfd5ccb033d4e1b2a7d7a36c211f8fd5fd3d4
Zlatin Rusev <zlatinrusev@gmail.com>
2024-09-19 10:53:49 +0200

    Changes Commited

--- exam_preps/music_app/music_app/web/urls.py
+++ exam_preps/music_app/music_app/web/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = ()
+from django.urls import path
+
+from music_app.web.views import index
+
+urlpatterns = (
+    path("", index, name="index"),
+)
--- exam_preps/music_app/music_app/web/views.py
+++ exam_preps/music_app/music_app/web/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from music_app.profiles.models import Profile
+
+
+def get_profile():
+    return Profile.objects.first()
+
+
+def create_profile(request):
+    return render(request, "web/home-no-profile.html")
+
+
+def index(request):
+    profile = get_profile()
+
+    if profile is None:
+        create_profile(request)
+
+    return render(request, "web/home-with-profile.html")
