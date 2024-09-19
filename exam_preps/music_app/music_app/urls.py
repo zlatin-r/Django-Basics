e595f35404c70472e5f80223672e0705ed60596e
Zlatin Rusev <zlatinrusev@gmail.com>
2024-09-19 10:27:15 +0200

    Changes Commited

--- exam_preps/music_app/music_app/urls.py
+++ exam_preps/music_app/music_app/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = (
     path('admin/', admin.site.urls),
-]
+
+    path("", include("music_app.web.urls")),
+    path("albums/", include("music_app.albums.urls")),
+    path("profiles/", include("music_app.profiles.urls")),
+)
