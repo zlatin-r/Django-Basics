0d0f45331c4b9aa99d547a42259aa419329fe2b2
zlatin-r <zlatinrusev@gmail.com>
2024-12-29 16:26:16 +0100

    Changes added

--- exam_preps/music_app/music_app/settings.py
+++ exam_preps/music_app/music_app/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'music_app.profiles.apps.ProfilesConfig'
 ]
 
 MIDDLEWARE = [
