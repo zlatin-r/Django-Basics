1900eb4679058a83e1f9c5a08ca16c8aebac0b97
zlatin-r <zlatinrusev@gmail.com>
2024-12-29 15:21:07 +0100

    Changes added

---
+++ exam_preps/backup/music_app/music_app/albums/views.py
@@ -0,0 +1,29 @@
+from django.urls import reverse_lazy
+from django.views import generic as views
+
+from common.profile_helpers import get_profile
+from music_app.albums.models import Album
+
+
+class AlbumFormViewMixin:
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+
+        form.fields["name"].widget.attrs["placeholder"] = "Album Name"
+        form.fields["artist_name"].widget.attrs["placeholder"] = "Artist"
+        form.fields["description"].widget.attrs["placeholder"] = "Description"
+        form.fields["image_url"].widget.attrs["placeholder"] = "Image URL"
+        form.fields["price"].widget.attrs["placeholder"] = "Price"
+
+        return form
+
+
+class CreateAlbumView(AlbumFormViewMixin, views.CreateView):
+    queryset = Album.objects.all()
+    fields = ("name", "artist_name", "genre", "description", "image_url", "price")
+    template_name = "albums/album-add.html"
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        form.instance.owner_id = get_profile().pk
+        return super().form_valid(form)
