2fd6c882d2a30ad5d040d61f6f0d76d1d7b43557
zlatin-r <zlatinrusev@gmail.com>
2024-12-29 16:44:53 +0100

    Changes added

---
+++ exam_preps/music_app/music_app/common/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from music_app.common.views import HomePage
+
+urlpatterns = [
+    path('', HomePage.as_view(), name='home'),
+]
--- exam_preps/music_app/music_app/urls.py
+++ exam_preps/music_app/music_app/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('music_app.common.urls')),
+    path('album/', include('music_app.albums.urls')),
+    path('profile/', include('music_app.profiles.urls')),
 ]
