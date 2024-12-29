b86dbe23de23a7061674fce9bb7ad0e377205142
zlatin-r <zlatinrusev@gmail.com>
2024-12-29 15:07:23 +0100

    Changes added

--- exam_preps/music_app/music_app/albums/views.py
+++ exam_preps/music_app/music_app/albums/views.py
@@ -8,7 +8,7 @@ from music_app.albums.models import Album
 class AlbumFormViewMixin:
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
-        # Can be moved a mixin
+
         form.fields["name"].widget.attrs["placeholder"] = "Album Name"
         form.fields["artist_name"].widget.attrs["placeholder"] = "Artist"
         form.fields["description"].widget.attrs["placeholder"] = "Description"
--- exam_preps/music_app/music_app/settings.py
+++ exam_preps/music_app/music_app/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
