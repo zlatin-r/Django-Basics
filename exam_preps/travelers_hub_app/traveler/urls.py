dc8a9818ff6613adbb83237309a7281c752ee3e1
zlatin-r <zlatinrusev@gmail.com>
2024-12-30 14:42:11 +0100

    Changes Added

--- exam_preps/travelers_hub_app/traveler/urls.py
+++ exam_preps/travelers_hub_app/traveler/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from traveler import views
 
+urlpatterns = [
+    path('create/', views.TravelerCreateView.as_view(), name='add-traveler'),
+    path('details/', views.TravelerDetailsView.as_view(), name='traveler-details'),
+    path('edit/', views.TravelerEditView.as_view(), name='traveler-edit'),
+    path('delete/', views.TravelerDeleteView.as_view(), name='traveler-delete')
 ]
--- exam_preps/travelers_hub_app/traveler/views.py
+++ exam_preps/travelers_hub_app/traveler/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+
+class TravelerCreateView(CreateView):
+    pass
+
+
+class TravelerDetailsView(DetailView):
+    pass
+
+
+class TravelerEditView(UpdateView):
+    pass
+
+
+class TravelerDeleteView(DeleteView):
+    pass
