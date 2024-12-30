8e980e57cd01d8bb19e672dc602decb25ae677e8
zlatin-r <zlatinrusev@gmail.com>
2024-12-30 14:43:10 +0100

    Changes Added

--- exam_preps/travelers_hub_app/trip/urls.py
+++ exam_preps/travelers_hub_app/trip/urls.py
@@ -4,7 +4,7 @@ from trip import views
 
 urlpatterns = [
     path('crete/', views.TripCreateView.as_view(), name='add-trip'),
-    path('<int: pk>', include([
+    path('<int:pk>', include([
         path('details/', views.TripDetailsView.as_view(), name='trip-details'),
         path('edit/', views.TripEditView.as_view(), name='trip-edit'),
         path('delete/', views.TripDeleteView.as_view(), name='delete-trip')
