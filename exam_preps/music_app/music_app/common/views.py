e46120d0ed488a6ab2b36c7a2502374bb9abb992
zlatin-r <zlatinrusev@gmail.com>
2024-12-30 11:38:21 +0100

    Changes added

--- exam_preps/music_app/music_app/common/views.py
+++ exam_preps/music_app/music_app/common/views.py
@@ -12,10 +12,6 @@ class HomePage(ListView, BaseFormView):
     form_class = ProfileCreateForm
     success_url = reverse_lazy('home')
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data()
-        context['profile'] = get_user_obj()
-        return context
 
     def get_template_names(self):
         profile = get_user_obj()  # None or QuerySet
