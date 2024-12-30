6762551635365d308d8e7ba525583a794b8f34ad
zlatin-r <zlatinrusev@gmail.com>
2024-12-30 16:13:29 +0100

    Changes Added

---
+++ exam_preps/travelers_hub_app/traveler/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from traveler.models import Traveler
+
+
+class TravelerBaseForm(forms.ModelForm):
+    class Meta:
+        model = Traveler
+        fields = '__all__'
+
+
+class TravelerCreateForm(TravelerBaseForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['nickname'].widget.attrs['placeholder'] = 'Enter a unique nickname...'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter a valid email address...'
+        self.fields['country'].widget.attrs['placeholder'] = 'Enter a country code like <BGR>...'
--- exam_preps/travelers_hub_app/traveler/views.py
+++ exam_preps/travelers_hub_app/traveler/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from traveler import models
+from traveler.forms import TravelerCreateForm
+from travelers_hub_app.utils import get_traveler_obj
 
 
 class TravelerCreateView(CreateView):
-    pass
+    model = models.Traveler
+    form_class = TravelerCreateForm
+    template_name = 'traveler/create-traveler.html'
+    success_url = reverse_lazy('common/all-trips.html')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['traveler'] = get_traveler_obj()  # Add custom context
+        return context
 
 
 class TravelerDetailsView(DetailView):
