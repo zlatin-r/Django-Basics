04c6e9595ea74c0918ddcbb1c58d62e53f0c38ed
zlatin-r <zlatinrusev@gmail.com>
2024-12-30 13:33:03 +0100

    Changes Added

--- exam_preps/travelers_hub_app/traveler/models.py
+++ exam_preps/travelers_hub_app/traveler/models.py
@@ -1,3 +1,39 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 
-# Create your models here.
+from traveler.validators import NicknameValidator
+
+
+class Traveler(models.Model):
+    MAX_LENGTH = 30
+    MIN_LENGTH =3
+
+    nickname = models.CharField(
+        unique=True,
+        max_length=MAX_LENGTH,
+        validators=(
+            MinLengthValidator(MIN_LENGTH),
+            NicknameValidator(),
+        ),
+        help_text="*Nicknames can contain only letters and digits.",
+        blank=False,
+        null=False,
+    )
+    email = models.EmailField(
+        max_length=MAX_LENGTH,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    country = models.CharField(
+        max_length=MIN_LENGTH,
+        validators=(
+            MaxLengthValidator(MIN_LENGTH),
+        ),
+        blank=False,
+        null=False,
+    )
+    about_me = models.TextField(
+        blank=True,
+        null=True,
+    )
--- exam_preps/travelers_hub_app/trip/models.py
+++ exam_preps/travelers_hub_app/trip/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from traveler.models import Traveler
+
+
+class Trip(models.Model):
+    destination = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100,
+        validators=(
+            MinLengthValidator(3),
+        )
+    )
+    summary = models.TextField(
+        blank=False,
+        null=False,
+    )
+    start_date = models.DateField(
+        blank=False,
+        null=False,
+    )
+    duration = models.PositiveSmallIntegerField(
+        blank=False,
+        null=False,
+        default=1,
+        help_text="*Duration in days is expected."
+    )
+    image_url = models.URLField(
+        blank=True,
+        null=True,
+    )
+    traveler = models.ForeignKey(
+        to=Traveler,
+        on_delete=models.CASCADE,
+    )
