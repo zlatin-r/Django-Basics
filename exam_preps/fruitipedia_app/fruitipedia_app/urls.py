3fcb5fbf83f68ebf332e2a74b450a293afa6bc73
zlatin-r <zlatinrusev@gmail.com>
2025-01-02 16:15:00 +0100

    Changes Added

--- exam_preps/fruitipedia_app/fruitipedia_app/urls.py
+++ exam_preps/fruitipedia_app/fruitipedia_app/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from fruitipedia_app.common import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePage.as_view(), name='home')
 ]
