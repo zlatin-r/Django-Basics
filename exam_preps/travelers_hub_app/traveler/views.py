f7459c6268893a62545df2580673b6074d212866
zlatin-r <zlatinrusev@gmail.com>
2025-01-02 13:19:39 +0100

    Changes Added

--- exam_preps/travelers_hub_app/traveler/views.py
+++ exam_preps/travelers_hub_app/traveler/views.py
@@ -50,4 +50,15 @@ class TravelerEditView(UpdateView):
 
 
 class TravelerDeleteView(DeleteView):
-    pass
+    model = Traveler
+    template_name = 'traveler/delete-traveler.html'
+    context_object_name = 'traveler'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return get_traveler_obj()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['traveler'] = get_traveler_obj()  # Add traveler to the context
+        return context
