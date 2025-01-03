2bfdbc831bd7582e54ab615e83054227aa3c5a6e
zlatin-r <zlatinrusev@gmail.com>
2025-01-03 15:32:00 +0100

    Changes Added

--- exam_preps/fruitipedia_app/fruitipedia_app/common/views.py
+++ exam_preps/fruitipedia_app/fruitipedia_app/common/views.py
@@ -1,9 +1,18 @@
 from django.views.generic import TemplateView
 
+from fruitipedia_app.fruits.models import Fruit
+from fruitipedia_app.utils import get_all_fruits
+
 
 class HomePage(TemplateView):
-    template_name = 'common/index.html'
+    template_name = 'index.html'
 
 
 class Dashboard(TemplateView):
-    template_name = 'common/dashboard.html'
+    model = Fruit
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['fruits'] = get_all_fruits()
+        return context
--- exam_preps/fruitipedia_app/fruitipedia_app/utils.py
+++ exam_preps/fruitipedia_app/fruitipedia_app/utils.py
@@ -1,5 +1,10 @@
+from fruitipedia_app.fruits.models import Fruit
 from fruitipedia_app.profiles.models import Profile
 
 
 def get_profile():
     return Profile.objects.first()
+
+
+def get_all_fruits():
+    return Fruit.objects.all()
