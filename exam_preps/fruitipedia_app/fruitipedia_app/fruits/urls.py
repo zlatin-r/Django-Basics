8a8852743e4efb662c9c153133a0f6bc5fe3beaf
zlatin-r <zlatinrusev@gmail.com>
2025-01-03 10:51:45 +0100

    Changes Added

--- exam_preps/fruitipedia_app/fruitipedia_app/fruits/urls.py
+++ exam_preps/fruitipedia_app/fruitipedia_app/fruits/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from fruitipedia_app.fruits import views
 
 urlpatterns = [
-    path('create/', views.FruitCreateView.as_view(), name='fruit-create'),
-    path('<int:fruit_id>/', include([
-        path('details/', views.FruitDetailsView, name='fruit-details'),
-        path('edit/', views.FruitEditView, name='fruit-edit'),
-        path('delete/', views.FruitDeleteView, name='fruit-delete'),]
-    ))
+    # path('create/', views.FruitCreateView.as_view(), name='fruit-create'),
+    # path('<int:fruit_id>/', include([
+    #     path('details/', views.FruitDetailsView, name='fruit-details'),
+    #     path('edit/', views.FruitEditView, name='fruit-edit'),
+    #     path('delete/', views.FruitDeleteView, name='fruit-delete'),]
+    # ))
 ]
--- exam_preps/fruitipedia_app/fruitipedia_app/urls.py
+++ exam_preps/fruitipedia_app/fruitipedia_app/urls.py
@@ -5,8 +5,7 @@ from fruitipedia_app.common import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', include('fruitipedia_app.common.urls')),
     path('profile/', include('fruitipedia_app.profiles.urls')),
     path('fruit/', include('fruitipedia_app.fruits.urls')),
-    path('', include('fruitipedia_app.common.urls')),
 ]
