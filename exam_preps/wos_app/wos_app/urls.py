ba77f434ce003bab8e46a47ea96e8977f08b9877
zlatin-r <zlatinrusev@gmail.com>
2025-01-04 16:51:12 +0100

    Changes Added

--- exam_preps/wos_app/wos_app/urls.py
+++ exam_preps/wos_app/wos_app/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
+    path('', include('wos_app.common.urls')),
+    path('car/', include('wos_app.cars.urls')),
+    path('profile/', include('wos_app.user_profile.urls')),
 ]
