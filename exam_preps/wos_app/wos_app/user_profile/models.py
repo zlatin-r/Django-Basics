be02c47c803e25ecbd46f975afba60fabd9161f2
zlatin-r <zlatinrusev@gmail.com>
2025-01-04 15:19:07 +0100

    Changes Added

--- exam_preps/wos_app/wos_app/user_profile/models.py
+++ exam_preps/wos_app/wos_app/user_profile/models.py
@@ -1,3 +1,11 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=15,
+        validators=[
+            MinLengthValidator(3, message="Username must be at least 3 chars long!")
+        ]
+    )
