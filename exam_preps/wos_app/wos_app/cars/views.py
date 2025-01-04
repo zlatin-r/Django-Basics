ffca8f706546466de741d22ad74b8d0015547f50
zlatin-r <zlatinrusev@gmail.com>
2025-01-04 18:34:01 +0100

    Changes Added

--- exam_preps/wos_app/wos_app/cars/views.py
+++ exam_preps/wos_app/wos_app/cars/views.py
@@ -3,9 +3,14 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from wos_app.cars.forms import CarCreateForm
+from wos_app.common.utils import get_profile
 
 
 class CarCreateView(CreateView):
     template_name = 'car/car-create.html'
     form_class = CarCreateForm
     success_url = reverse_lazy('catalogue')
+
+    def form_valid(self, form):
+        form.instance.owner = get_profile()
+        return super().form_valid(form)
