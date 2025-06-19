72f7b2bea8cc5287f35900183c1afe3a7ac226a2
zlatin-r <zlatinrusev@gmail.com>
2025-06-19 18:14:09 +0200

    changes are made

---
+++ exam_preps/travelers_hub_app2/common/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from core.utils import get_profile
+
+
+def index(request):
+    traveler = get_profile()
+    context = {
+        'traveler': traveler
+    }
+    return render(request, template_name='common/index.html', context=context)
