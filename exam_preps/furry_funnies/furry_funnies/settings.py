7477beb3e6edf5c9f842a5ff061b98125f242793
zlatin-r <zlatinrusev@gmail.com>
2025-06-20 16:49:03 +0200

    Initial Commit

--- exam_preps/furry_funnies/furry_funnies/settings.py
+++ exam_preps/furry_funnies/furry_funnies/settings.py
@@ -29,6 +29,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+PROJECT_APPS = [
+    'author',
+    'posts',
+    'common',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
