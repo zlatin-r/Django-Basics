4a48f641860cc152ad3b003382701ab0ff356076
zlatin-r <zlatinrusev@gmail.com>
2025-06-20 16:45:26 +0200

    Initial Commit

---
+++ exam_preps/furry_funnies/furry_funnies/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for furry_funnies project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furry_funnies.settings')
+
+application = get_asgi_application()
